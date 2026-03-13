de2bad202e30b24180aedb87a036f9cbfae785d4
kseniavas <114919991+kseniavas@users.noreply.github.com>
2026-03-13 11:42:27 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -7,55 +7,58 @@ namespace WinFormsApp1
             InitializeComponent();
         }
         float sinX;
-        float sum;
-        int cnt;
-        float e_;
-        float x;
+        float sum;//сумма ряда
+       
+        float e_;//точность (Eps)
+        float x;//аргумет функции
+        int n;//количество членов ряда
+        //вычисление факториала
         int factorial(int n)
         {
             if (n < 0) return 0;
             if (n == 0) return 1;
             return n * factorial(n - 1);
         }
-        //float sumOfRow(float x, float e)
-        //{
-        //    float sum = 0;
-        //    int n = 2;
-        //    float elem1=(float)((Math.Pow((-1), 0)* Math.Pow(x, 2*0))/(factorial(2*0+1)));
-        //    float elem2 = (float)((Math.Pow((-1), 1) * Math.Pow(x, 2 * 1)) / (factorial(2 * 1 + 1)));
-        //    sum += (elem1 + elem2);
-        //    while (elem1-elem2<e)
-        //    {
 
-        //        elem1 = (float)((Math.Pow((-1), n) * Math.Pow(x, 2 * n)) / (factorial(2 * n + 1)));
-        //        elem2 = (float)((Math.Pow((-1), n+1) * Math.Pow(x, 2 * n+1)) / (factorial(2 * n+1 + 1))); 
-        //        sum += (elem1 + elem2);
-        //        n++;
-        //    }
-        //    return sum*x;
-        //}
+        //вычисление n-ного элемента ряда
+        float inRes(float x, int n)
+        {
+            return (float)((Math.Pow((-1), n) * Math.Pow(x, 2 * n)) / (factorial(2 * n + 1)));
+        }
+
+        //вычисление суммы ряда и количества элементов ряда
+        //принимает аргумент x и точность e
+        //возвращает кортеж (сумма, количество элементов)
+        (float sumres , int nres) sumOfRow(float x, float e)
+        {
+            float sum = 0;
+            int n = 2;
+            float elem1 = inRes(x, 0);
+            float elem2 = inRes(x, 1);
+            sum += (elem1 + elem2);
+            while (elem1 - elem2 < e)
+            {
+
+                elem1 = inRes(x, n);
+                elem2 = inRes(x, n+1);
+                sum += (elem1 + elem2);
+                n++;
+            }
+            return (sum * x, n);
+        }
+       
+        //кнопка "вычислить"
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.TextLength!=0 && textBox2.TextLength != 0)
+            if (textBox1.TextLength!=0 && textBox2.TextLength != 0&&!float.IsInfinity(x))//проверка на заполненность полей
             {
-                float sum = 0;
-                int n = 2;
-                float elem1 = (float)((Math.Pow((-1), 0) * Math.Pow(x, 2 * 0)) / (factorial(2 * 0 + 1)));
-                float elem2 = (float)((Math.Pow((-1), 1) * Math.Pow(x, 2 * 1)) / (factorial(2 * 1 + 1)));
-                sum += (elem1 + elem2);
-                while (elem1 - elem2 < e_)
-                {
-
-                    elem1 = (float)((Math.Pow((-1), n) * Math.Pow(x, 2 * n)) / (factorial(2 * n + 1)));
-                    elem2 = (float)((Math.Pow((-1), n + 1) * Math.Pow(x, 2 * n + 1)) / (factorial(2 * n + 1 + 1)));
-                    sum += (elem1 + elem2);
-                    n++;
-                }
-                sum = sum * x;
+                (float, int) sumCount = sumOfRow(x, e_);
+                sum = sumOfRow(x, e_).sumres;//вычисление суммы ряда
+                n = sumOfRow(x, e_).nres;
                 sinX = (float)Math.Sin(x);
                 label4.Text = "Sin(x) = " + sinX;
-                label5.Text = "����� ���� " + sum;
-                label6.Text = "���������� ������ ���� " + n;
+                label5.Text = "Сумма ряда " + sum;
+                label6.Text = "Количество членов ряда " + n;
             }
 
         }
@@ -77,19 +80,21 @@ namespace WinFormsApp1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if(textBox1.TextLength>0)
+            
+            if(float.TryParse(textBox1.Text, out float res))
                 e_ = float.Parse(textBox1.Text);
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            if (textBox2.TextLength > 0)
+            if (float.TryParse(textBox1.Text, out float res))
                 x = float.Parse(textBox2.Text);
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && !(e.KeyChar == '-' && textBox1.TextLength == 0) && !(e.KeyChar == ',' && textBox1.TextLength == 0))
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && !(e.KeyChar == '-' && textBox1.TextLength == 0) && !(e.KeyChar == ',' ))
             {
                 e.Handled = true;
             }
@@ -97,7 +102,7 @@ namespace WinFormsApp1
 
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && !(e.KeyChar == '-' && textBox2.TextLength == 0) && !(e.KeyChar == ',' && textBox1.TextLength == 0))
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && !(e.KeyChar == ',' ))//нельзя ввести отрицательное число, т.к. знак "-" не подразумевается
             {
                 e.Handled = true;
             }
