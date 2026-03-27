4bdfe3b7fc635963d596f6effdc8a341a6775034
Ваше Имя <tomgf46@gmail.com>
2026-03-27 05:55:00 +0300

    Первый коммит

---
+++ WinFormsApp1/DATABASE1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+   class DATABASE1
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=ADCLG1; Initial Catalog=Vaschishina_MSServer; Integrated Security=True;Encrypt=True;TrustServerCertificate=True;
+");
+        public void openConnection()
+        {
+            sqlConnection.Open();
+        }
+        public void closeConnection()
+        {
+            sqlConnection.Close();
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+    
+}
